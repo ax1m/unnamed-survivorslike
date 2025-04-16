c7779ff4f8611640cb276e77a82e3fdd65bd14eb
ax1m <axim92@gmail.com>
2025-04-16 12:27:59 +0200

    Initial commit

---
+++ scripts/enemy.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 40
+
+@onready var player: CharacterBody2D = $"../Player"
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	velocity = (player.position - position).normalized() * SPEED
+	
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
+		
+	move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player.hit()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 60.0
+
+var hitpoints = 1
+
+@onready var sprite = $AnimatedSprite2D
+@onready var timer: Timer = $"../Timer"
+@onready var label: Label = $Label
+
+func _physics_process(delta: float) -> void:
+
+	velocity = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+		).normalized() * SPEED
+	
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
+		
+	if velocity.length() > 0:
+		sprite.play("run")
+	else:
+		sprite.play("idle")
+
+	move_and_slide()
+	
+func hit() -> void:
+	hitpoints -= 1
+	if hitpoints == 0:
+		label.visible = true
+		Engine.time_scale = 0.5
+		set_physics_process(false)
+		sprite.play("deth")
+		timer.start()
+	elif hitpoints > 0:
+		sprite.play("hit")
+	
